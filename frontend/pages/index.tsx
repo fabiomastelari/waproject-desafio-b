d0d7cbe805583b56b6669a0a1dc802afc9e751d7
Fábio Mastelari <fabio.mastelari@gmail.com>
2022-11-23 14:35:07 -0300

    Initial frontent next + typescript empty homepage app

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title="Empty Homepage">
+    <h1>This is just a empty Homepage</h1>
+  </Layout>
+)
+
+export default IndexPage
