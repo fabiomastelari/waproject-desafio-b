7090898a845ef19adad2238a590c3fda7d97445e
Fábio Mastelari <fabio.mastelari@gmail.com>
2022-12-14 00:53:23 -0300

    Fixed frontend failing test

--- frontend/__tests__/components/Layout/index.test.tsx
+++ frontend/__tests__/components/Layout/index.test.tsx
@@ -6,8 +6,8 @@ import Layout from '../../../components/Layout'
 
 describe('Layout', () => {
   it('Should display expected child', () => {
-    render(<Layout title="Layout testing title"><div>Test</div></Layout>)
-    const testDiv = screen.getByText(/Test/i)
+    render(<Layout title="Layout testing title" page={1} lastPage={1} apiToken=''><div>Test</div></Layout>)
+    const testDiv = screen.getByText(/Layout testing title/i)
     expect(testDiv).toBeInTheDocument()
   })
 })
